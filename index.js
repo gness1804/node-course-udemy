922b00b6630d0d3f06cfde1373ac83a21e6559bd
gness1804 <graham.nessler@gmail.com>
2018-07-09 18:15:27 -0500

    Add long running process to demonstrate single thread latency

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+const doWork = (duration) => {
+  const start = Date.now();
+  while (Date.now() - start < duration) {
+
+  }
+}
+
 app.get('/', (req, res) => {
-  res.send('Hi!');
+  doWork(5000);
+  res.send('Hello!');
 });
 
 app.listen(3000, () => {
