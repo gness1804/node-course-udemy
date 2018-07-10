8ffaa2c4bfece726b3590399dc7893fc0eeb6cb0
gness1804 <graham.nessler@gmail.com>
2018-07-09 19:59:50 -0500

    Get clustering working

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const runServer = () => {
     res.send('Hello!');
   });
 
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+  });
+
   app.listen(3000, () => {
     console.log('server running on port 3000.');
   });
@@ -21,7 +25,11 @@ const runServer = () => {
 // is the file being executed in master mode?
 if (cluster.isMaster) {
   //will cause index.js to be executed again, but in child mode
-  cluster.fork();
+  let counter = 1
+  while (counter <= 4) {
+    cluster.fork();
+    counter++;
+  }
 } else {
   // running in child mode. this will run the server code and do nothing else
   runServer();
